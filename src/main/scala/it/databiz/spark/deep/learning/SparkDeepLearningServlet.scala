207a961fc9483f81af3ab2d976de82b67ebf4506
Vincibean <andrebessi00@gmail.com>
2016-05-08 21:03:32 +0100

    Added HTML page + JS

--- src/main/scala/it/databiz/spark/deep/learning/SparkDeepLearningServlet.scala
+++ src/main/scala/it/databiz/spark/deep/learning/SparkDeepLearningServlet.scala
@@ -5,12 +5,8 @@ import org.scalatra._
 class SparkDeepLearningServlet extends DeepLearningOnApacheSparkMnistStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    ssp("index", "title" -> "MNIST")
   }
 
 }
